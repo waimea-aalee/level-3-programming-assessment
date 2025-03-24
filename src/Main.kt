6ad0546b801bce32e6d0d255e7ab4f41dccf6b57
Azaria <aalee@waimea.school.nz>
2025-03-25 09:59:44 +1300

    Added an instruction popup

--- src/Main.kt
+++ src/Main.kt
@@ -58,6 +58,9 @@ class App() {
 class MainWindow(val app: App) : JFrame(), ActionListener {
 
     // Fields to hold the UI elements
+    private lateinit var PopUp: PopUpDialog
+    private lateinit var openButton: JButton
+
     private lateinit var clicksLabel: JLabel
     private lateinit var locationDesc: JLabel
 
@@ -101,6 +104,16 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         val descFont = Font(Font.SANS_SERIF, Font.ITALIC, 20)
         val buttonFont = Font(Font.SANS_SERIF, Font.PLAIN, 20)
 
+        PopUp = PopUpDialog()
+
+        openButton = JButton("Instructions")
+        openButton.bounds = Rectangle(425, 20, 150, 50)
+        openButton.font = buttonFont
+        openButton.addActionListener(this)
+        openButton.background = Color.WHITE
+        openButton.setForeground(Color.BLACK)
+        add(openButton)
+
         clicksLabel = JLabel("Location")
         clicksLabel.horizontalAlignment = SwingConstants.CENTER
         clicksLabel.bounds = Rectangle(50, 0, 500, 100)
@@ -112,28 +125,38 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         locationDesc.font = descFont
         add(locationDesc)
 
+// DIRECTION BUTTONS --------------------------------------------------------------
+
         northButton = JButton("North")
         northButton.bounds = Rectangle(250,300,100,50)
         northButton.font = buttonFont
         northButton.addActionListener(this)     // Handle any clicks
+        northButton.background = Color.WHITE
+        northButton.setForeground(Color.BLACK)
         add(northButton)
 
         eastButton = JButton("East")
         eastButton.bounds = Rectangle(350,350,100,50)
         eastButton.font = buttonFont
         eastButton.addActionListener(this)     // Handle any clicks
+        eastButton.background = Color.WHITE
+        eastButton.setForeground(Color.BLACK)
         add(eastButton)
 
         southButton = JButton("South")
         southButton.bounds = Rectangle(250,400,100,50)
         southButton.font = buttonFont
         southButton.addActionListener(this)     // Handle any clicks
+        southButton.background = Color.WHITE
+        southButton.setForeground(Color.BLACK)
         add(southButton)
 
         westButton = JButton("West")
         westButton.bounds = Rectangle(150,350,100,50)
         westButton.font = buttonFont
         westButton.addActionListener(this)     // Handle any clicks
+        westButton.background = Color.WHITE
+        westButton.setForeground(Color.BLACK)
         add(westButton)
     }
 
@@ -153,12 +176,41 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
      */
     override fun actionPerformed(e: ActionEvent?) {
         when (e?.source) {
+            openButton -> {
+                PopUp.isVisible = true
+            }
             northButton -> {
                 app.updateClickCount()
                 updateView()
             }
         }
     }
-
 }
 
+class PopUpDialog(): JDialog() {
+
+    init {
+        configureWindow()
+        addControls()
+        setLocationRelativeTo(null)
+    }
+
+    private fun configureWindow() {
+        title = "Instructions"
+        contentPane.preferredSize = Dimension(400, 200)
+        isResizable = false
+        isModal = true
+        layout = null
+        contentPane.background = Color.BLACK
+        pack()
+    }
+    private fun addControls() {
+        val basefont = Font(Font.SANS_SERIF, Font.PLAIN, 16)
+
+        val message = JLabel("<html>You are in a school.<br><br>Your goal is to navigate it using the direction buttons, and find and use all the keys throughout the school in order to escape.")
+        message.bounds = Rectangle(25,25,350,150)
+        message.verticalAlignment = SwingConstants.TOP
+        message.font = basefont
+        add(message)
+    }
+}
